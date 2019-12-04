81254fcb6bce2cbe2984bb9318e2e1a6e45c1eda
zackerymarkusrayspears <zackery.spears97@gmail.com>
2019-12-04 14:51:37 -0600

    Changed className to background in app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import Home from './Home/Home';
 import Navbars from './Components/Navbars/Navbars';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 function App() {
   return (
-    <div className='container'>
+    <div className='background'>
       <Navbars />
       <Home />
     </div>
